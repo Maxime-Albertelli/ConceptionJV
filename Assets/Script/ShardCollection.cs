99b376d7ca359c8ebf8fa565fa6a4ad379c2d133
Maxime Albertelli <91539486+Maxime-Albertelli@users.noreply.github.com>
2026-04-09 11:52:10 +0200

    Ajout de l'UI in game pour la récolte des shards

--- Assets/Script/ShardCollection.cs
+++ Assets/Script/ShardCollection.cs
@@ -1,16 +1,20 @@
+using TMPro;
 using UnityEngine;
 
 public class ShardCollection : MonoBehaviour
 {
     private int shardValue = 0;
+    [SerializeField] private TextMeshProUGUI shardText;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.transform.tag == "Shard")
+        if(other.CompareTag("Shard"))
         {
+            other.enabled = false;
             shardValue++;
             Debug.Log("Shard collected");
             Destroy(other.gameObject);
+            shardText.text = "Shards : " + shardValue.ToString();
         }
         
     }
