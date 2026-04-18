a9b99f25f83ad8e120df6d908fe68a9a543361b6
Henristote <gicquel.henri@gmail.com>
2026-04-18 23:41:51 +0200

    ✨ Procedural Planet 1rst phase

---
+++ Assets/Script/NoiseSettings.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using UnityEngine;
+
+[System.Serializable]
+public class NoiseSettings
+{
+    public enum FilterType { Simple, Ridgid }
+    public FilterType filterType;
+
+    [ConditionalHide("filterType", 0)]
+    public SimpleNoiseSettings simpleNoiseSettings;
+    public RidgidNoiseSettings ridgidNoiseSettings;
+
+    [System.Serializable]
+    public class SimpleNoiseSettings
+    {
+        public float strength = 1f;
+        [Range(1, 8)]
+        public int numLayers = 1;
+        public float baseRoughness = 1f;
+        public float roughness = 1f;
+        public float persistence = 0.5f;
+        public Vector3 centre;
+        public float minValue;
+    }
+
+    [System.Serializable]
+    public class RidgidNoiseSettings : SimpleNoiseSettings
+    {
+        public float weightMultiplier = 0.8f;
+    }
+}
